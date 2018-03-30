bf1ab2e3c2fd7d0cf07c2cb6daa3a44a0af57f4f
David Hart <dphart@rochester.rr.com>
2018-03-30 14:46:23 -0400

    Initial Commit

---
+++ ZitiPacketTunnel/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ZitiPacketTunnel
+//
+//  Created by David Hart on 3/30/18.
+//  Copyright © 2018 David Hart. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ZitiPacketTunnel/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ZitiPacketTunnel
+//
+//  Created by David Hart on 3/30/18.
+//  Copyright © 2018 David Hart. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
