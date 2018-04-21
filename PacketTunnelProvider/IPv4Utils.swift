41571cdeea194c04e39b8b3ca16a2f86c5a22151
David Hart <dphart@rochester.rr.com>
2018-04-21 18:33:30 -0400

    Initial version

---
+++ PacketTunnelProvider/IPv4Utils.swift
@@ -0,0 +1,43 @@
+//
+//  IPv4Utils.swift
+//  PacketTunnelProvider
+//
+//  Created by David Hart on 4/20/18.
+//  Copyright © 2018 David Hart. All rights reserved.
+//
+
+import Foundation
+
+// convenience..
+class IPv4Utils {
+    static func extractUInt16(_ data:Data, from:Int) -> UInt16 {
+        let byteArray = [UInt8](data.subdata(in: from..<(from+2)))
+        return CFSwapInt16(UnsafePointer(byteArray).withMemoryRebound(to:UInt16.self, capacity: 1) {
+            $0.pointee
+        })
+    }
+
+    static func updateUInt16(_ data: inout Data, at:Int, value:UInt16) {
+        var swapped = CFSwapInt16(value)
+        data.replaceSubrange(at..<(at+2), with: withUnsafeBytes(of: &swapped) { Array($0) })
+    }
+    
+    static func payloadToString(_ payload: Data) -> String {
+        var i = 0
+        var s = payload.map {
+            var str = String(format: "%02X", $0);
+            if (i%4)==0 { str += "\n"}; i+=1
+            return str
+        }.joined(separator: " ")
+        
+        let unicodeScalars:[String] = payload.map  {
+            if $0 < 0x21 || $0 > 0x7f {
+                return String(UnicodeScalar("."))
+            } else {
+                return String(UnicodeScalar($0))
+            }
+        }
+        s += "\n \(unicodeScalars.joined(separator: " "))"
+        return s
+    }
+}
