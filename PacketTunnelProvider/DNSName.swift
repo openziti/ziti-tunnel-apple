148d615514aeb6c219019667a8aef0e0b6993ac9
David Hart <dphart@rochester.rr.com>
2018-04-25 07:01:05 -0400

    Refactor DNS support into multiple files

---
+++ PacketTunnelProvider/DNSName.swift
@@ -0,0 +1,77 @@
+//
+//  DNSName.swift
+//  PacketTunnelProvider
+//
+//  Created by David Hart on 4/25/18.
+//  Copyright © 2018 David Hart. All rights reserved.
+//
+
+import Foundation
+
+class DNSName : NSObject {
+    var nameString = ""
+    var numBytes = 0
+    
+    init(_ data:Data, offset:Int) {
+        super.init()
+        
+        var count = 0
+        var keepCountingBytes = true
+        var done = false
+        var currOffset = offset
+        
+        while !done {
+            let countOrOffset:UInt8 = data[data.startIndex + currOffset]
+            
+            if isAnOffset(countOrOffset) {
+                currOffset = Int(countOrOffset)
+                count = findCountAndOffset(data, offset:&currOffset)
+                if keepCountingBytes {
+                    self.numBytes += 2
+                }
+                keepCountingBytes = false
+            } else {
+                count = Int(countOrOffset)
+                if keepCountingBytes {
+                    self.numBytes += (count + 1)
+                }
+            }
+            
+            if count == 0 {
+                done = true
+            } else {
+                if !self.nameString.isEmpty {
+                    self.nameString += "."
+                }
+                
+                let indx = data.startIndex + currOffset + 1
+                for i in indx..<(count+indx) {
+                    self.nameString += String(UnicodeScalar(data[i]))
+                }
+                currOffset += (count + 1)
+            }
+        }
+    }
+    
+    init(_ nameString:String) {
+        self.nameString = nameString
+        self.numBytes = nameString.count + 2 // leading count, trailing 0x00
+    }
+    
+    private func isAnOffset(_ value:UInt8) -> Bool {
+        // if first two bits are set, we have a new offset
+        return (value & 0xc0) == 0xc0
+    }
+    
+    private func findCountAndOffset(_ data:Data, offset: inout Int) -> Int {
+        
+        let countOrOffset:UInt8 = data[data.startIndex + offset]
+        if isAnOffset(countOrOffset) {
+            // new offset is made from last 14 bits of two bytes
+            offset = Int(IPv4Utils.extractUInt16(data, from: data.startIndex + offset) & 0x3f)
+            return findCountAndOffset(data, offset:&offset)
+        } else {
+            return Int(countOrOffset)
+        }
+    }
+}
---
+++ PacketTunnelProvider/DNSResourceRecord.swift
@@ -0,0 +1,122 @@
+//
+//  DNSResourceRecord.swift
+//  PacketTunnelProvider
+//
+//  Created by David Hart on 4/25/18.
+//  Copyright © 2018 David Hart. All rights reserved.
+//
+
+import Foundation
+
+typealias DNSQuestion = DNSResoureRecodeBase
+
+class DNSResoureRecodeBase : NSObject {
+    
+    static let minNumBytes = 5
+    
+    let name:DNSName
+    let recordType:DNSRecordType
+    let recordClass:DNSRecordClass
+    var numBytes = 0
+    
+    init?(_ data:Data, offset:Int) {
+        
+        if data.count < DNSQuestion.minNumBytes {
+            NSLog("Invalid data for DNS Question")
+            return nil
+        }
+        
+        self.name = DNSName(data, offset:offset)
+        
+        let rTypeIndx = data.startIndex + self.name.numBytes
+        self.recordType = DNSRecordType(IPv4Utils.extractUInt16(data, from: rTypeIndx))
+        self.recordClass = DNSRecordClass(IPv4Utils.extractUInt16(data, from: rTypeIndx + MemoryLayout<UInt16>.size))
+        self.numBytes = self.name.numBytes + (MemoryLayout<UInt16>.size * 2)
+    }
+    
+    init(_ nameString:String, recordType:DNSRecordType, recordClass:DNSRecordClass) {
+        self.name = DNSName(nameString)
+        self.recordType = recordType
+        self.recordClass = recordClass
+        self.numBytes = self.name.numBytes + (MemoryLayout<UInt16>.size * 2)
+    }
+    
+    func toBytes() -> Data {
+        var data = Data()
+        
+        let segments:[String] = self.name.nameString.components(separatedBy: ".")
+        segments.forEach { seg in
+            data.append(UInt8(seg.count))
+            data.append(contentsOf:seg.utf8)
+        }
+        data.append(0x00)
+        IPv4Utils.appendUInt16(&data, value: self.recordType.rawValue)
+        IPv4Utils.appendUInt16(&data, value: self.recordClass.rawValue)
+        
+        return data
+    }
+    
+    override var debugDescription: String {
+        return "   > " + self.name.nameString + " type: \(self.recordType), class \(self.recordClass)\n"
+    }
+}
+
+class DNSResourceRecord : DNSResoureRecodeBase {
+    
+    var ttl:UInt32 = 0
+    var resourceDataLength:UInt16 = 0
+    var resourceData:Data? = nil
+    
+    override init?(_ data:Data, offset:Int) {
+        super.init(data, offset:offset)
+        
+        var indx = data.startIndex + offset + self.numBytes
+        
+        self.ttl = IPv4Utils.extractUInt32(data, from: indx)
+        self.numBytes += MemoryLayout<UInt32>.size
+        indx += MemoryLayout<UInt32>.size
+        
+        self.resourceDataLength = IPv4Utils.extractUInt16(data, from: indx)
+        self.numBytes += MemoryLayout<UInt16>.size
+        indx += MemoryLayout<UInt16>.size
+        
+        let rdl = Int(self.resourceDataLength)
+        self.resourceData = data[indx..<(indx + rdl)]
+        self.numBytes += rdl
+    }
+    
+    init(_ nameString:String, recordType:DNSRecordType, recordClass:DNSRecordClass, ttl:UInt32, resourceData:Data?) {
+        super.init(nameString, recordType:recordType, recordClass:recordClass)
+        self.ttl = ttl
+        
+        self.numBytes += MemoryLayout<UInt32>.size + MemoryLayout<UInt16>.size // ttl and recordLength
+        
+        if let data = resourceData {
+            self.resourceData = data
+            self.resourceDataLength = UInt16(data.count)
+            self.numBytes += data.count
+        }
+    }
+    
+    override func toBytes() -> Data {
+        var data = super.toBytes()
+        IPv4Utils.appendUInt32(&data, value: self.ttl)
+        IPv4Utils.appendUInt16(&data, value: self.resourceDataLength)
+        
+        if let rd = self.resourceData {
+            data.append(rd)
+        }
+        return data
+    }
+    
+    override var debugDescription: String {
+        var s =  "   > " + self.name.nameString + " type: \(self.recordType), class \(self.recordClass)\n"
+        s += "      ttl: \(self.ttl)\n"
+        s += "      resourceDataLength: \(self.resourceDataLength)\n"
+        
+        if let data = self.resourceData {
+            s += IPv4Utils.payloadToString(data)
+        }
+        return s
+    }
+}
