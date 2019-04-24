43ae4f9a10ecf62d29635a9459900f3958b8cef6
David Hart <dphart@rochester.rr.com>
2019-04-24 07:25:12 -0400

    About dialog/credits...

--- ZitiPacketTunnel/MainMenuBar.swift
+++ ZitiPacketTunnel/MainMenuBar.swift
@@ -14,14 +14,27 @@ class MainMenuBar : NSObject, NSWindowDelegate {
     
     let appName = Bundle.main.infoDictionary?[kCFBundleNameKey as String] as? String ?? "Ziti"
     let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
-    var tunStatusItem:NSMenuItem?
-    var tunConnectItem:NSMenuItem?
-    var showDocItem:NSMenuItem?
+    private var tunStatusItem:NSMenuItem!
+    private var tunConnectItem:NSMenuItem!
+    private var showDocItem:NSMenuItem!
     
     private override init() {
         statusItem.button?.image = NSImage(named:NSImage.Name("StatusBarConnected"))
         super.init()
-        statusItem.menu = updateMenu()
+        let menu = NSMenu()
+        tunStatusItem = newMenuItem(title:"Status:", action:#selector(MainMenuBar.noop(_:)))
+        menu.addItem(tunStatusItem)
+        tunConnectItem = newMenuItem(title:"Connect", action:#selector(MainMenuBar.connect(_:)))
+        menu.addItem(tunConnectItem)
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(newMenuItem(title: "Manage Tunnels", action: #selector(MainMenuBar.showPanel(_:))))
+        showDocItem = newMenuItem(title: "Show In Dock", action: #selector(MainMenuBar.showInDock(_:)))
+        showDocItem.state = .on
+        menu.addItem(showDocItem)
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(newMenuItem(title: "About \(appName)", action: #selector(MainMenuBar.about(_:))))
+        menu.addItem(newMenuItem(title: "Quit \(appName)", action: #selector(MainMenuBar.quit(_:)), keyEquivalent: "q"))
+        statusItem.menu = menu
         getMainWindow()?.delegate = self
         TunnelMgr.shared.tsChangedCallbacks.append(self.tunnelStatusDidChange)
     }
@@ -29,27 +42,27 @@ class MainMenuBar : NSObject, NSWindowDelegate {
     func tunnelStatusDidChange(_ status:NEVPNStatus) {
         switch status {
         case .connecting:
-            tunStatusItem?.title = "Status: Connecting..."
-            tunConnectItem?.title = "Disconnect"
+            tunStatusItem.title = "Status: Connecting..."
+            tunConnectItem.title = "Disconnect"
             break
         case .connected:
-            tunStatusItem?.title = "Status: Connected"
-            tunConnectItem?.title = "Disconnect"
+            tunStatusItem.title = "Status: Connected"
+            tunConnectItem.title = "Disconnect"
             statusItem.button?.image = NSImage(named:NSImage.Name("StatusBarConnected"))
             break
         case .disconnecting:
-            tunStatusItem?.title = "Status: Disconnecting..."
+            tunStatusItem.title = "Status: Disconnecting..."
             break
         case .disconnected:
-            tunStatusItem?.title = "Status: Not Connected"
-            tunConnectItem?.title = "Connect"
+            tunStatusItem.title = "Status: Not Connected"
+            tunConnectItem.title = "Connect"
             statusItem.button?.image = NSImage(named:NSImage.Name("StatusBarDisconnected"))
             break
         case .invalid:
             print("Invalid")
             break
         case .reasserting:
-            tunStatusItem?.title = "Status: Reasserting..."
+            tunStatusItem.title = "Status: Reasserting..."
             break
         @unknown default:
             print("Unknown...")
@@ -66,16 +79,17 @@ class MainMenuBar : NSObject, NSWindowDelegate {
     func updateMenu() -> NSMenu {
         let menu = NSMenu()
         tunStatusItem = newMenuItem(title:"Status:", action:#selector(MainMenuBar.noop(_:)))
-        menu.addItem(tunStatusItem!)
+        menu.addItem(tunStatusItem)
         tunConnectItem = newMenuItem(title:"Connect", action:#selector(MainMenuBar.connect(_:)))
-        menu.addItem(tunConnectItem!)
+        menu.addItem(tunConnectItem)
         menu.addItem(NSMenuItem.separator())
         menu.addItem(newMenuItem(title: "Manage Tunnels", action: #selector(MainMenuBar.showPanel(_:))))
         showDocItem = newMenuItem(title: "Show In Dock", action: #selector(MainMenuBar.showInDock(_:)))
-        showDocItem?.state = .on
-        menu.addItem(showDocItem!)
+        showDocItem.state = .on
+        menu.addItem(showDocItem)
         menu.addItem(NSMenuItem.separator())
-        menu.addItem(newMenuItem(title: "Quit \(appName)", action: #selector(MainMenuBar.quit(_:)), keyEquivalent: "q"))
+        menu.addItem(newMenuItem(title: "About \(appName)", action: #selector(MainMenuBar.about(_:))))
+        menu.addItem(newMenuItem(title: "Quit \(appName)", action: #selector(MainMenuBar.quit(_:)), keyEquivalent: "q"))        
         return menu
     }
     
@@ -97,13 +111,13 @@ class MainMenuBar : NSObject, NSWindowDelegate {
     @objc func showInDock(_ sender: Any?) {
         if NSApp.activationPolicy() == .regular {
             NSApp.setActivationPolicy(.accessory)
-            showDocItem?.state = .off
+            showDocItem.state = .off
             if getMainWindow()?.isVisible ?? false {
                 DispatchQueue.main.async { self.showPanel(self) }
             }
         } else {
             NSApp.setActivationPolicy(.regular)
-            showDocItem?.state = .on
+            showDocItem.state = .on
         }
     }
     
@@ -115,11 +129,16 @@ class MainMenuBar : NSObject, NSWindowDelegate {
     }
     
     @objc func connect(_ sender: Any?) {
-        if tunConnectItem?.title == "Connect" {
+        if tunConnectItem.title == "Connect" {
             try? TunnelMgr.shared.startTunnel()
         } else {
             TunnelMgr.shared.stopTunnel()
         }
+    }
+    
+    @objc func about(_ sender: Any?) {
+        NSApp.activate(ignoringOtherApps: true)
+        NSApp.orderFrontStandardAboutPanel(self)
     }
     
     @objc func quit(_ sender: Any?) {
