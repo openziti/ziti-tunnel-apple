bcd8252c1991d3e074a8713f167b800f02033d02
Jeremy Tellier <jeremy.tellier@netfoundry.io>
2020-07-02 11:03:39 -0400

    UI Project Targets
    Created UI Projects for unified interface applications

---
+++ Ziti-UI-Mac/ContentView.swift
@@ -0,0 +1,19 @@
+//
+// Copyright © 2019 NetFoundry Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
