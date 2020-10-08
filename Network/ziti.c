1c3efddff09c2d4168239d0f9ea8757a7599bc01
Dave Hart <dphart@rochester.rr.com>
2020-10-08 16:24:35 -0400

    wrap ziti_sdk_c_host_v1 to handle type mismatch

---
+++ Network/ziti.c
@@ -0,0 +1,23 @@
+//
+// Copyright 2019-2020 NetFoundry, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#include <stdio.h>
+#include "ziti/ziti_tunnel_cbs.h"
+
+void ziti_sdk_c_host_v1_wrapper(void *ziti_ctx, uv_loop_t *loop, const char *service_id, const char *proto, const char *hostname, int port) {
+    ziti_sdk_c_host_v1(ziti_ctx, loop, service_id, proto, hostname, port);
+}
+
