d15357c837e246d592b215ee4523e322dcfd929b
Jeremy Tellier <jeremy.tellier@netfoundry.io>
2020-11-12 20:22:24 -0500

    StoryBoards and Seques / Empty View Controllers

---
+++ ZitiMobilePacketTunnel/Controls/IdentityRenderer.swift
@@ -0,0 +1,37 @@
+//
+// Copyright 2019-2020 NetFoundry, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import UIKit
+
+class IdentityRenderer: UIStackView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
