1ddc53c555917a57db433c439c14767bd9b2e0be
Dave Hart <dphart@rochester.rr.com>
2022-05-10 22:25:19 -0400

    Fix typo

--- ZitiMobilePacketTunnel/SnapshotViewController.swift
+++ ZitiMobilePacketTunnel/SnapshotViewController.swift
@@ -43,7 +43,7 @@ class SnapshotViewController: UIViewController, UIActivityItemSource {
     }
     
     func activityViewController(_ activityViewController: UIActivityViewController, subjectForActivityType activityType: UIActivity.ActivityType?) -> String {
-        return "Ziti Mobile Edge Snapsot"
+        return "Ziti Mobile Edge Snapshot"
     }
 
 }
