61212cf8c32656f564968b3f7b1a26ca8fa384a1
Dave Hart <dphart@rochester.rr.com>
2022-06-23 16:37:25 -0400

    LowPowerMode config UI

--- ZitiMobilePacketTunnel/TunnelSettingsViewController.swift
+++ ZitiMobilePacketTunnel/TunnelSettingsViewController.swift
@@ -18,24 +18,77 @@ import UIKit
 import NetworkExtension
 import CZiti
 
-class TunnelSettingsCell: UITableViewCell {
+protocol CellResettable {
+    func reset()
+}
+class LowPowerModeCell: UITableViewCell, CellResettable {
+    weak var tsvc:TunnelSettingsViewController?
+    var origValue = false
+    var requiresRestart = false
+    
+    @IBOutlet weak var lowPowerSwitch: UISwitch!
+    
+    var onChanged:((Bool) -> Void)?
+    @IBAction func switchChanged(_ sender: Any) {
+        if requiresRestart { tsvc?.restartRequired = true }
+        tsvc?.resetBtn?.isEnabled = true
+        onChanged?(lowPowerSwitch.isOn)
+    }
+    
+    func setup(_ tsvc:TunnelSettingsViewController, _ value:Bool, _ requiresRestart:Bool, _ onChanged: @escaping (Bool)->Void) {
+        self.tsvc = tsvc
+        self.lowPowerSwitch.isOn = value
+        self.origValue = value
+        self.requiresRestart = requiresRestart
+        self.onChanged = onChanged
+    }
+    
+    func reset() {
+        lowPowerSwitch.isOn = origValue
+    }
+}
+
+class TunnelSettingsCell: UITableViewCell, CellResettable {
+    weak var tsvc:TunnelSettingsViewController?
+    var origValue:String?
+    var requiresRestart = false
+    
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var value: UITextField!
     
-    var onChanged:((TunnelSettingsCell) -> Void)?
+    var onChanged:((String) -> Void)?
     @IBAction func editingChanged(_ sender: Any) {
-        onChanged?(self)
+        if requiresRestart { tsvc?.restartRequired = true }
+        tsvc?.resetBtn?.isEnabled = true
+        onChanged?(value.text ?? "")
+    }
+    
+    func setup(_ tsvc:TunnelSettingsViewController, _ label:String, _ value:String, _ requiresRestart:Bool, _ onChanged: @escaping (String)->Void) {
+        self.tsvc = tsvc
+        self.label.text = label
+        self.origValue = value
+        self.value.text = value
+        self.requiresRestart = requiresRestart
+        self.onChanged = onChanged
+    }
+    
+    func reset() {
+        self.value.text = self.origValue
     }
 }
 
 class TunnelSettingsViewController: UITableViewController {
     weak var tvc:TableViewController?
-    var saveBtn:UIBarButtonItem?
+    var resetBtn:UIBarButtonItem?
+    var restartRequired = false
+    
+    let defaults = ProviderConfig()
     
     var ip:String = "0.0.0.0"
     var mask:String = "255.0.0.0"
     var mtu:String = "0"
     var dns:String = "0.0.0.0"
+    var lowPowerMode = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,21 +104,31 @@ class TunnelSettingsViewController: UITableViewController {
                 self.mask = mask
                 self.mtu = mtu
                 self.dns = dns
+                self.lowPowerMode = conf[ProviderConfig.LOW_POWER_MODE_KEY] as? Bool ?? defaults.lowPowerMode
             }
         }
         
-        saveBtn = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(onSave))
-        navigationItem.rightBarButtonItem = saveBtn
-        saveBtn?.isEnabled = false
+        resetBtn = UIBarButtonItem(title: "Reset", style: .done, target: self, action: #selector(onReset))
+        navigationItem.rightBarButtonItem = resetBtn
+        resetBtn?.isEnabled = false
     }
     
-    @objc func onSave() {
+    @objc func onReset() {
+        for c in tableView.visibleCells {
+            if let rc = c as? CellResettable {
+                rc.reset()
+            }
+        }
+    }
+    
+    @objc func saveConfig() {
         var dict = ProviderConfigDict()
         dict[ProviderConfig.IP_KEY] = ip
         dict[ProviderConfig.SUBNET_KEY] = mask
         dict[ProviderConfig.MTU_KEY] = mtu
         dict[ProviderConfig.DNS_KEY] = dns
         dict[ProviderConfig.LOG_LEVEL_KEY] = String(ZitiLog.getLogLevel().rawValue)
+        dict[ProviderConfig.LOW_POWER_MODE_KEY] = lowPowerMode
         
         let conf:ProviderConfig = ProviderConfig()
         if let error = conf.parseDictionary(dict) {
@@ -91,12 +154,48 @@ class TunnelSettingsViewController: UITableViewController {
                     alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default))
                     self.tvc?.present(alert, animated: true, completion: nil)
                 } else {
-                    self.tvc?.tunnelMgr.restartTunnel()
-                    self.navigationController?.popViewController(animated: true)
+                    if self.restartRequired {
+                        self.tvc?.tunnelMgr.restartTunnel()
+                    } else {
+                        self.tvc?.tunnelMgr.reassert()
+                    }
+                    //self.navigationController?.popViewController(animated: true)
                 }
             }
         }
     }
+    
+    // TODO: never gets called...
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // if nothing changed, we're done
+        if !(resetBtn?.isEnabled ?? false) {
+            return
+        }
+        
+        if validate() == false {
+            let alert = UIAlertController(
+                title:"Configuration Error",
+                message: "Invalid configuration settings, configuration will not be stored",
+                preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default))
+            tvc?.present(alert, animated: true, completion: nil)
+            return
+        }
+        saveConfig()
+    }
+    
+//    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+//        // Is this the base place to do this, or should it be done on viewWillDisappear? Problem with doing it there is we can't cancel it
+//        // if the inputs are invalid. Downside of doing it here is if user edits and then leaves the app the changes won't take effect.  Downside
+//        // of an explit Save button is that users hit Back expecting the changes have taken effect (especially the lowPowerMode toggle)
+//        var performSegue = true
+//        if resetBtn?.isEnabled ?? false {
+//            performSegue =  onSave()
+//        }
+//        return performSegue
+//    }
     
     func validate() -> Bool {
         guard IPUtils.isValidIpV4Address(ip) else { return false }
@@ -109,7 +208,7 @@ class TunnelSettingsViewController: UITableViewController {
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 2
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -124,41 +223,43 @@ class TunnelSettingsViewController: UITableViewController {
         var cell:UITableViewCell?
         
         // Configure the cell...
-        //if indexPath.section == 0 {
+        if indexPath.section == 0 {
             cell = tableView.dequeueReusableCell(withIdentifier: "TUNNEL_SETTING_CELL", for: indexPath)
             if let settingsCell = cell as? TunnelSettingsCell {
                 if indexPath.row == 0 {
-                    settingsCell.label.text = "IP Address"
-                    settingsCell.value.text = ip
-                    settingsCell.onChanged = { [weak self] sc in
-                        self?.ip = sc.value.text ?? ""
-                        self?.saveBtn?.isEnabled = self?.validate() ?? false
+                    settingsCell.setup(self, "IP Address", ip, true) { [weak self] value in
+                        self?.ip = value
                     }
                     settingsCell.value.becomeFirstResponder()
                 } else if indexPath.row == 1 {
-                    settingsCell.label.text = "Subnet Mask"
-                    settingsCell.value.text = mask
-                    settingsCell.onChanged = { [weak self] sc in
-                        self?.mask = sc.value.text ?? ""
-                        self?.saveBtn?.isEnabled = self?.validate() ?? false
+                    settingsCell.setup(self, "Subnet Mask", mask, true) { [weak self] value in
+                        self?.mask = value
                     }
                 } else if indexPath.row == 2 {
-                    settingsCell.label.text = "MTU"
-                    settingsCell.value.text = mtu
-                    settingsCell.onChanged = { [weak self] sc in
-                        self?.mtu = sc.value.text ?? ""
-                        self?.saveBtn?.isEnabled = self?.validate() ?? false
+                    settingsCell.setup(self, "MTU", mtu, false) { [weak self] value in
+                        self?.mtu = value
                     }
                 } else {
-                    settingsCell.label.text = "DNS Server"
-                    settingsCell.value.text = dns
-                    settingsCell.onChanged = { [weak self] sc in
-                        self?.dns = sc.value.text ?? ""
-                        self?.saveBtn?.isEnabled = self?.validate() ?? false
+                    settingsCell.setup(self, "DNS Server", dns, true) { [weak self] value in
+                        self?.dns = value
                     }
                 }
             }
-        //}
+        } else {
+            cell = tableView.dequeueReusableCell(withIdentifier: "LOW_POWER_MODE_CELL", for: indexPath)
+            if let lowPowerCell = cell as? LowPowerModeCell {
+                lowPowerCell.setup(self, lowPowerMode, false) { [weak self] value in
+                    self?.lowPowerMode = value
+                }
+            }
+        }
         return cell! // Don't let this happen!
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        if section == 1 {
+            return "Disable all Ziti communications and traffic targeted for the tunnel (including DNS) when device not in use"
+        }
+        return nil
     }
 }
