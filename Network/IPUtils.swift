84943caea4dc56f869ba1b9e32a471c12a3b836f
Dave Hart <dphart@rochester.rr.com>
2022-06-30 20:19:57 -0400

    autoreleasepool for string components and trimming

--- Network/IPUtils.swift
+++ Network/IPUtils.swift
@@ -21,41 +21,55 @@ import NetworkExtension
 class IPUtils {
     static func ipV4AddressStringToData(_ ipString:String) -> Data {
         var data = Data()
-        let ipParts:[String] = ipString.components(separatedBy: ".")
-        ipParts.forEach { part in
-            let b = UInt8((part as NSString).integerValue)
-            data.append(b)
+        autoreleasepool {
+            let ipParts:[String] = ipString.components(separatedBy: ".")
+            ipParts.forEach { part in
+                let b = UInt8((part as NSString).integerValue)
+                data.append(b)
+            }
         }
         return data
     }
     
     static func isValidIpV4Address(_ str:String) -> Bool {
-        let addr = str.trimmingCharacters(in: .whitespaces)
-        let parts = addr.components(separatedBy: ".")
-        let nums = parts.compactMap { Int($0) }
-        return parts.count == 4 && nums.count == 4 && nums.filter { $0 >= 0 && $0 < 256}.count == 4
+        var isValid = false
+        autoreleasepool {
+            let addr = str.trimmingCharacters(in: .whitespaces)
+            let parts = addr.components(separatedBy: ".")
+            let nums = parts.compactMap { Int($0) }
+            isValid =  parts.count == 4 && nums.count == 4 && nums.filter { $0 >= 0 && $0 < 256}.count == 4
+        }
+        return isValid
     }
     
     static func areSameRoutes(_ r1:NEIPv4Route, _ r2:NEIPv4Route) -> Bool {
-        let r1A = ipV4AddressStringToData(r1.destinationAddress)
-        let r1M = ipV4AddressStringToData(r1.destinationSubnetMask)
-        let r2A = ipV4AddressStringToData(r2.destinationAddress)
-        let r2M = ipV4AddressStringToData(r2.destinationSubnetMask)
-        
-        for ((r1A, r1M), (r2A, r2M)) in zip(zip(r1A, r1M), zip(r2A, r2M)) {
-            if (r1A & r1M) != (r2A & r2M) {
-                return false
+        var areSame = true
+        autoreleasepool {
+            let r1A = ipV4AddressStringToData(r1.destinationAddress)
+            let r1M = ipV4AddressStringToData(r1.destinationSubnetMask)
+            let r2A = ipV4AddressStringToData(r2.destinationAddress)
+            let r2M = ipV4AddressStringToData(r2.destinationSubnetMask)
+            
+            for ((r1A, r1M), (r2A, r2M)) in zip(zip(r1A, r1M), zip(r2A, r2M)) {
+                if (r1A & r1M) != (r2A & r2M) {
+                    areSame = false
+                    break
+                }
             }
         }
-        return true
+        return areSame
     }
     
     static func inV4Subnet(_ dest:Data, network:Data, mask:Data) -> Bool {
-        for (dest, (network, mask)) in zip(dest, zip(network, mask)) {
-            if (dest & mask) != (network & mask) {
-                return false
+        var inSubnet = true
+        autoreleasepool {
+            for (dest, (network, mask)) in zip(dest, zip(network, mask)) {
+                if (dest & mask) != (network & mask) {
+                    inSubnet = false
+                    break
+                }
             }
         }
-        return true
+        return inSubnet
     }
 }
