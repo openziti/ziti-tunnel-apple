6963c3b32499de26c38214eff5eb4b2127024828
Shawn Carey <shawn.carey@netfoundry.io>
2023-12-07 15:48:04 -0500

    add quit item to manubar extra. remove application dock menu.

--- ZitiPacketTunnel/AppDelegate.swift
+++ ZitiPacketTunnel/AppDelegate.swift
@@ -27,11 +27,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         zLog.info(Version.verboseStr)
     }
     
-    func applicationDockMenu(_ sender: NSApplication) -> NSMenu? {
-        // hack to similate menuBar being opened
-        menuBar?.menuDidBeginTracking(n: Notification(name: Notification.Name(rawValue: "DockMenuOpened")))
-        return menuBar?.statusItem.menu
-    }
+//    func applicationDockMenu(_ sender: NSApplication) -> NSMenu? {
+//        // hack to simulate menuBar being opened
+//        menuBar?.menuDidBeginTracking(n: Notification(name: Notification.Name(rawValue: "DockMenuOpened")))
+//        return menuBar?.statusItem.menu
+//    }
     
     func applicationWillFinishLaunching(_ notification: Notification) {
         let center = UNUserNotificationCenter.current()
